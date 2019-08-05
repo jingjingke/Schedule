a5d783725b93e63c04ac8a53da6548ad07f9e850
cuiziyan <919948650@qq.com>
2019-08-05 17:55:59 +0800

    计算日程用时

--- app/src/main/java/com/jingjingke/schedule/ScheduleDatabase.java
+++ app/src/main/java/com/jingjingke/schedule/ScheduleDatabase.java
@@ -1,6 +1,5 @@
 package com.jingjingke.schedule;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -35,18 +34,7 @@ public class ScheduleDatabase {
                 "from schedule as s inner join status as i on s.status_id=i.id where s.status_id<4 order by s.create_time desc", null);
         if (cursor.moveToFirst()) {
             do {
-                int id = cursor.getInt(cursor.getColumnIndex("id"));
-                String name = cursor.getString(cursor.getColumnIndex("name"));
-                String content = cursor.getString(cursor.getColumnIndex("content"));
-                String remark = cursor.getString(cursor.getColumnIndex("remark"));
-                String cost = cursor.getString(cursor.getColumnIndex("cost_time"));
-                String status = cursor.getString(cursor.getColumnIndex("status"));
-                int sid = cursor.getInt(cursor.getColumnIndex("sid"));
-                long create_time = cursor.getLong(cursor.getColumnIndex("create_time"));
-                String create = simpleDateFormat.format(new Date(create_time));
-
-                Schedule schedule = new Schedule(id, name, content, remark, status, sid, create, cost);
-                list.add(schedule);
+                list.add(extractionOfSchedule(cursor));
             } while (cursor.moveToNext());
         }
 
@@ -61,18 +49,7 @@ public class ScheduleDatabase {
                 "from schedule as s inner join status as i on s.status_id=i.id where i.name=? order by s.create_time desc", new String[]{status_name});
         if (cursor.moveToFirst()) {
             do {
-                int id = cursor.getInt(cursor.getColumnIndex("id"));
-                String name = cursor.getString(cursor.getColumnIndex("name"));
-                String content = cursor.getString(cursor.getColumnIndex("content"));
-                String remark = cursor.getString(cursor.getColumnIndex("remark"));
-                String cost = cursor.getString(cursor.getColumnIndex("cost_time"));
-                String status = cursor.getString(cursor.getColumnIndex("status"));
-                int sid = cursor.getInt(cursor.getColumnIndex("sid"));
-                long create_time = cursor.getLong(cursor.getColumnIndex("create_time"));
-                String create = simpleDateFormat.format(new Date(create_time));
-
-                Schedule schedule = new Schedule(id, name, content, remark, status, sid, create, cost);
-                list.add(schedule);
+                list.add(extractionOfSchedule(cursor));
             } while (cursor.moveToNext());
         }
 
@@ -93,7 +70,6 @@ public class ScheduleDatabase {
         return list;
     }
 
-
     // 新增日程后返回当前自增id
     public int addSchedule(String title, String content, String remark, int status) {
         long time = new Date().getTime();
@@ -113,17 +89,7 @@ public class ScheduleDatabase {
         Cursor cursor = database.rawQuery("select s.id,s.name,s.content,s.remark,s.create_time,s.cost_time,i.name as status,i.id as sid " +
                 "from schedule as s inner join status as i on s.status_id=i.id where s.id=? limit 1", new String[]{String.valueOf(schedule_id)});
         if (cursor.moveToFirst()) {
-            int id = cursor.getInt(cursor.getColumnIndex("id"));
-            String name = cursor.getString(cursor.getColumnIndex("name"));
-            String content = cursor.getString(cursor.getColumnIndex("content"));
-            String remark = cursor.getString(cursor.getColumnIndex("remark"));
-            String cost = cursor.getString(cursor.getColumnIndex("cost_time"));
-            String status = cursor.getString(cursor.getColumnIndex("status"));
-            int sid = cursor.getInt(cursor.getColumnIndex("sid"));
-            long create_time = cursor.getLong(cursor.getColumnIndex("create_time"));
-            String create = simpleDateFormat.format(new Date(create_time));
-
-            schedule = new Schedule(id, name, content, remark, status, sid, create, cost);
+            schedule = extractionOfSchedule(cursor);
         }
         return schedule;
     }
@@ -164,7 +130,7 @@ public class ScheduleDatabase {
 
 
     // 开始一个进度
-    public void addProgress(int schedule_id, String remark) {
+    private void addProgress(int schedule_id, String remark) {
         long time = new Date().getTime();
         database.execSQL("insert into progress (schedule_id,remark,start_time,end_time) values (?,?,?,?);",
                 new Object[]{schedule_id, remark, time, 0});
@@ -172,15 +138,66 @@ public class ScheduleDatabase {
 
     // 结束一个进度
     private void endProgress(int schedule_id) {
-        Cursor cursor = database.rawQuery("select id from progress where schedule_id=? order by start_time desc limit 1", new String[]{String.valueOf(schedule_id)});
-        String rid = "";
+        Cursor cursor = database.rawQuery("select s.cost_time,p.id,p.start_time from progress as p inner join schedule as s on s.id=p.schedule_id where schedule_id=? order by start_time desc limit 1", new String[]{String.valueOf(schedule_id)});
         if (cursor.moveToFirst()) {
-            rid = Integer.toString(cursor.getInt(0));
-            ContentValues values = new ContentValues();
+            int id = cursor.getInt(cursor.getColumnIndex("id"));
+            long start = cursor.getLong(cursor.getColumnIndex("start_time"));
+            long cost_time = cursor.getLong(cursor.getColumnIndex("cost_time"));
             long time = new Date().getTime();
-            values.put("end_time", time);
-            database.update("progress", values, "id=?", new String[]{rid});
+            cost_time = cost_time + (time - start);
+            database.execSQL("update progress set end_time=? where id=?", new Object[]{time, id});
+            database.execSQL("update schedule set cost_time=? where id=?", new Object[]{cost_time, schedule_id});
         }
+    }
+
+
+    // 根本日程Id取得最后一条开始时间与当前时间差
+    private long getLastCostTime(int schedule_id) {
+        long cost = 0;
+        Cursor cursor = database.rawQuery("select start_time from progress where schedule_id=? order by start_time desc limit 1", new String[]{String.valueOf(schedule_id)});
+        if (cursor.moveToFirst()) {
+            long start_time = cursor.getLong(cursor.getColumnIndex("start_time"));
+            long time = new Date().getTime();
+            cost = time - start_time;
+        }
+        return cost;
+    }
+
+    // 提取每一条查询得出的日程数据
+    private Schedule extractionOfSchedule(Cursor cursor) {
+        int id = cursor.getInt(cursor.getColumnIndex("id"));
+        String name = cursor.getString(cursor.getColumnIndex("name"));
+        String content = cursor.getString(cursor.getColumnIndex("content"));
+        String remark = cursor.getString(cursor.getColumnIndex("remark"));
+        String status = cursor.getString(cursor.getColumnIndex("status"));
+        int sid = cursor.getInt(cursor.getColumnIndex("sid"));
+
+        long cost_time = cursor.getLong(cursor.getColumnIndex("cost_time"));
+        if (sid == 2) {
+            cost_time = getLastCostTime(id);
+        }
+        String cost = getCostText(cost_time);
+
+        long create_time = cursor.getLong(cursor.getColumnIndex("create_time"));
+        String create = simpleDateFormat.format(new Date(create_time));
+
+        Schedule schedule = new Schedule(id, name, content, remark, status, sid, create, cost);
+        return schedule;
+    }
+
+    // 工具--返回用时文本
+    private String getCostText(long time) {
+        long day, hour, minute, second;
+        time = time / 1000;
+        day = time / 86400;
+        hour = (time % 86400) / 3600;
+        minute = (time % 86400 % 3600) / 60;
+        second = (time % 86400 % 3600 % 60);
+        // 过滤无用数据
+        return (day == 0 ? "" : (day + "天")) +
+                (day + hour == 0 ? "" : (hour + "小时")) +
+                (day + hour + minute == 0 ? "" : (minute + "分")) +
+                (day + hour + minute + second == 0 ? "--" : (second + "秒"));
     }
 
 }
